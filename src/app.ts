4e34b34bbbd5b43dcd194410089178b96c7c57a8
twqDaniell <thewickedqueend@gmail.com>
2025-01-29 21:13:35 +0200

    itay's try to fix my code

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,26 @@
 import initApp from "./server";
 import appInit from "./server";
-const port = process.env.PORT || 433;
+const port = 4000;
 import https from "https";
 import http from "http";
 import fs from "fs";
 
-const tmpFunc = async () => {
-  console.log("Starting server...");
-  console.log("PORT", port);
-  
-  
+console.log("Starting server...");
+console.log("PORT", port);
 
-  initApp().then((app) => {
-    if (process.env.NODE_ENV !== "production") {
-      http.createServer(app).listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-      });
-    } else {
-      const httpsOptions = {
-        key: fs.readFileSync("./client-key.pem"),
-        cert: fs.readFileSync("./client-cert.pem"),
-      };
+initApp().then((app) => {
+  if (process.env.NODE_ENV !== "production") {
+    http.createServer(app).listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } else {
+    const httpsOptions = {
+      key: fs.readFileSync("./client-key.pem"),
+      cert: fs.readFileSync("./client-cert.pem"),
+    };
 
-      https.createServer(httpsOptions, app).listen(port, () => {
-        console.log(`Server is running on https://localhost:${port}`);
-      });
-    }
-  });
-};
-
-tmpFunc();
+    https.createServer(httpsOptions, app).listen(port, () => {
+      console.log(`Server is running on https://localhost:${port}`);
+    });
+  }
+});
